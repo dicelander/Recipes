c88104df02b0d040771526e5581c73ff574eddb7
dicelander <victorsander@gmail.com>
2023-05-22 09:47:53 -0300

    Adiciona .gitignore a raiz do projeto

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import { userRouter } from "./routes/users.js";
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+app.use('/auth', userRouter);
+
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT ${PORT}`)))
+    .catch((error) => console.log(error));
