912bc31823f4a8e0605ff40e735e647eeddf9de1
dicelander <victorsander@gmail.com>
2023-05-24 11:12:07 -0300

    Faz index.js procurar .env na raiz do projeto

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import path from path;
 
 import { userRouter } from "./routes/users.js";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 app.use('/auth', userRouter);
 
