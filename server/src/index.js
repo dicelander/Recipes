05bdbf00fc20364e96b896a1054794982fd1eb76
dicelander <victorsander@gmail.com>
2023-05-24 11:36:36 -0300

    Adiciona porta padrão 3000 caso ñ esteja definida

--- server/src/index.js
+++ server/src/index.js
@@ -2,18 +2,17 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import path from path;
 
 import { userRouter } from "./routes/users.js";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+dotenv.config();
 
 app.use('/auth', userRouter);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`SERVER RUNNING ON PORT ${PORT}`)))
